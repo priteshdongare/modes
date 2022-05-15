ae4eb0d3eddbea5a601c80a3668955bca954a183
Pritesh Dongare <72661897+priteshdongare@users.noreply.github.com>
2022-05-15 11:54:53 +0530

    Add files via upload

---
+++ js/user.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('#open').click(function(){
+      $('.nav-navigation').css('display','block');
+    });
+    $('#close').click(function(){
+      $('.nav-navigation').css('display','none');
+    });
+
+});
